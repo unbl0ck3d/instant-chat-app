3f2cd15d3c15002d91fbf3219ab506680c727ac0
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-17 14:23:55 +0530

    Create basic boilerplate for express

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 // Server side code for instant chat app
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", function(req, res){
+    res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(3000, function(){
+    console.log("listening to localhost 3000");
+})
