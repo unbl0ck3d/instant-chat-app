030395a82bfd96de90897b72b0d191b5c66b434d
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-17 15:31:29 +0530

    Add server side JavaScript for socket

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// Server side code for instant chat app
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/");
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(3000, function(){
-    console.log("listening to localhost 3000");
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
