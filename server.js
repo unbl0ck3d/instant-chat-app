9e29bd4e099c24f777d529d8934624ef88a47763
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-17 16:00:33 +0530

    Add message emit and receiving socket to server side

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on("disconnect", () =>{
-      console.log("A user disconnected");
-  })
+    console.log('a user connected');
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    })
+    socket.on("chat message", (msg)=>{
+        console.log("message received.");
+    })
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
