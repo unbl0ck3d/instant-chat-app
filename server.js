ff3cf877eba972dc45665d532ead88c34e4c34f2
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-07-21 09:16:42 +0530

    Change the whole structure of the page; Add dark theme

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const socketio = require("socket.io");
+
+const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = socketio(server);
+
 
 app.use(express.static(__dirname + "/"));
 
@@ -11,30 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    socket.emit('chat message', "Welcome to Chatbud");
-
-    console.log('a user connected');
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    })
-    socket.on("chat message", (msg) => {
-        console.log("message received.");
-        console.log("message: " + msg);
-    })
-    socket.on("message sender", (nick) => {
-        console.log("nick: " + nick);
-    })
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        socket.emit(msg);
-    });
-    socket.on('message sender', (nick) => {
-        io.emit('message sender', nick);
-    });
-});
+io.on("connection", function(socket) {
+    console.log("new connection");
+    socket.emit("welcomeMessage", "Welcome to Chatbud!")
+})
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
